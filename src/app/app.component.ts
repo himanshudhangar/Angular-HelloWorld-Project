ebd1fb0ee3913363a8f72be8c38b3c318a578da4
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-11-16 21:24:57 +0530

    Added:Hello world App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HelloWorld';
+  imgUrl="";
+  url: string | URL | undefined;
+  userName:string="";
+  nameError:string="";
+
+  ngOnInit():void{
+    this.title="Hello From Bridgelabz!"
+    this.imgUrl="/assets/Bridgelabzz.jpg";
+    this.url="https://www.bridgelabz.com";
+  }
+  onClick($event: any){
+    console.log("Save button is clicked",$event);
+    window.open(this.url,"_blank")
+  }
+  onInput($event:any){
+    console.log("Getting Input");
+    console.log("Change Event Occurred!", $event.data);
+    const nameRegex=RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError="";
+      return;
+    }
+    this.nameError="Name is Incorrect!"
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DemoComponent } from './components/demo/demo.component';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    DemoComponent
+   
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
